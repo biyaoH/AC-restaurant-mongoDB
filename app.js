404ee2bab6d6ea0624dbd6977cbf636672c78b31
Yao <biyao.hsu@gmail.com>
2020-02-24 16:19:53 +0100

    Add Mongoose connection and create Restaurant mondel

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 
 const port = 3000
+
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const Restaurant = require('./models/restaurant')
 
 app.get('/', (req, res) => {
   res.send('Restaurant MongoDB')
