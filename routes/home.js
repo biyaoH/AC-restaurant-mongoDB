676e02c094de3412297485752c201fef096a6b4f
Yao <biyao.hsu@gmail.com>
2020-02-27 22:59:00 +0100

    Add sort feature on index page from data req.query

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,36 @@ const router = express.Router()
 const Restaurant = require('../models/restaurant')
 
 router.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .exec((err, restaurants) => {
-      if (err) return console.error(err)
-      return res.render('index', { restaurants: restaurants })
-    })
+  // console.log(req.query.value)
+  const dropdown = req.query.value
+
+  if (dropdown === 'az') {
+    Restaurant.find()
+      .sort({ name: 'asc' })
+      .lean()
+      .exec((err, restaurants) => {
+        if (err) return console.error(err)
+        return res.render('index', { restaurants: restaurants })
+      })
+  } else if (dropdown === 'za') {
+    Restaurant.find()
+      .sort({ name: 'desc' })
+      .lean()
+      .exec((err, restaurants) => {
+        if (err) return console.error(err)
+        return res.render('index', { restaurants: restaurants })
+      })
+  } else {
+
+    Restaurant.find()
+      .lean()
+      .exec((err, restaurants) => {
+        if (err) return console.error(err)
+        return res.render('index', { restaurants: restaurants })
+      })
+  }
+
+
 
 })
 
