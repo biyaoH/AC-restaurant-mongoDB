121297acf7306a71b9410c6a7bbbae84f9b72952
Yao <biyao.hsu@gmail.com>
2020-03-03 14:56:24 +0100

    Add user registration feature

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(methodOverride('_method'))
 app.use('/', require('./routes/home.js'))
 //當路徑是/restaurants的時間執行後面的callback函數
 app.use('/restaurants', require('./routes/restaurant.js'))
-app.use('/user', require('./routes/user.js'))
+app.use('/users', require('./routes/user.js'))
 
 
 mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
