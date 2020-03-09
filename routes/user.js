a895f0705857f877ecaacfe115989bcb3c8f03c1
Yao <biyao.hsu@gmail.com>
2020-03-09 11:19:21 +0100

    Add connect-flash for UI messages

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const User = require('../models/user')
-
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
 
+const User = require('../models/user')
 // 登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
@@ -33,47 +32,64 @@ router.post('/register', (req, res) => {
   // const password2 = req.body.password2
   const { name, email, password, password2 } = req.body
 
-  User.findOne({ email: email }).then(user => {
-    if (user) {
-      console.log('User already exists')
-      res.render('register', {
-        name,
-        email,
-        password,
-        password2
-      })
-    } else {
-      const newUser = new User({
-        name,
-        email,
-        password
-      })
-      console.log(newUser)
+  let errors = []
 
-      bcrypt.genSalt(10, (err, salt) =>
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err
-          newUser.password = hash
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: '所有欄位都是必填' })
+    console.log('所有欄位都是必填')
+  }
+  if (password !== password2) {
+    errors.push({ message: '密碼輸入錯誤' })
+  }
 
-          console.log(newUser)
-          newUser
-            .save()
-            .then(user => {
-              res.redirect('/')
-            })
-            .catch(err => console.log(err))
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ message: '這個 Email 已經註冊過了' })
+        res.render('register', {
+          name,
+          email,
+          password,
+          password2
+        })
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
         })
 
-      )
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
 
-    }
-
-  })
+            console.log(newUser)
+            newUser
+              .save()
+              .then(user => {
+                res.redirect('/')
+              })
+              .catch(err => console.log(err))
+          })
+        )
+      }
+    })
+  }
 })
 
 // 登出
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出')
   res.redirect('/users/login')
 })
 
