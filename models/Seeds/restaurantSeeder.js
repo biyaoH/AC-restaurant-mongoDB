a758c6c8d6407d7f76269108f7fab4f7ef1fe86b
Yao <biyao.hsu@gmail.com>
2020-03-09 17:58:53 +0100

    modify files seeds

--- models/Seeds/restaurantSeeder.js
+++ models/Seeds/restaurantSeeder.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 const restaurantData = require('../../restaurant.json')
+const userData = require('../user.json')
 const Restaurant = require('../restaurant.js')
+const User = require('../user.js')
 
-mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true })
+mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 
 const db = mongoose.connection
 
@@ -12,8 +14,31 @@ db.on('error', () => {
 
 db.once('open', () => {
   console.log('db connected!')
-  const { results } = restaurantData
+  const { restaurantList } = restaurantData
+  const { userList } = userData
   // console.log(results)
-  results.forEach(result => Restaurant.create(result))
+  userList.forEach(user => User.create(user))
+  // results.forEach(result => Restaurant.create(result))
+
+  User.findOne({ email: 'user1@example.com' })
+    .then(user => {
+      if (user) {
+        for (let i = 0; i < 3; i++) {
+          // console.log(user)
+          restaurantList[i].userId = user._id
+          Restaurant.create(restaurantList[i])
+        }
+      }
+
+    })
+
+  User.findOne({ email: 'user2@example.com' })
+    .then(user => {
+      for (let i = 2; i < 6; i++) {
+        restaurantList[i].userId = user._id
+        Restaurant.create(restaurantList[i])
+      }
+    })
+
   console.log('done')
 })
